6cd20abb7ee7affe3bb120210c2c293775eea30f
dinesh-bazgain <dinesh.bazgain@gmail.com>
2025-09-03 01:20:37 +0545

    created a nest project, connected the postgressql to the project, added the prisma schema, created user/catogories/tickets/wards/comments module with services and controller

---
+++ gunaso-system/src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
+import { WardsModule } from './wards/wards.module';
+import { TicketsModule } from './tickets/tickets.module';
+import { CommentsModule } from './comments/comments.module';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [UsersModule, CategoriesModule, WardsModule, TicketsModule, CommentsModule, PrismaModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
