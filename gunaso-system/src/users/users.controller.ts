307f456946c5a5a33fa3321412dc4dcb12b820d9
dinesh-bazgain <dinesh.bazgain@gmail.com>
2025-09-07 11:19:32 +0545

    feat: add the feature to find the users in the system with specific id, findall, and self

--- gunaso-system/src/users/users.controller.ts
+++ gunaso-system/src/users/users.controller.ts
@@ -1,11 +1,25 @@
-import { UseGuards, Controller, Get, Request } from '@nestjs/common';
+import { UseGuards, Controller, Get, Request, Param } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @UseGuards(JwtAuthGuard)
-  @Get('me')
+  @Get('self')
   getProfile(@Request() req) {
     return req.user;
   }
+
+  @Get()
+  async findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(id);
+  }
+
 }
--- gunaso-system/src/users/users.service.ts
+++ gunaso-system/src/users/users.service.ts
@@ -1,4 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    return this.prismaService.user.create({
+      data: {
+        fullName: createUserDto.fullName,
+        email: createUserDto.email,
+        password: createUserDto.password,
+        role: createUserDto.role,
+      },
+    });
+  }
+
+  async findAll() {
+    return this.prismaService.user.findMany({
+      select: {
+        id: true,
+        fullName: true,
+        email: true,
+        role: true,
+        wardId: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+  }
+
+  async findOne(id: string) {
+    return this.prismaService.user.findUnique({
+      where: { id },
+    });
+  }
+
+  async update(id: string, updateUserDto: Partial<CreateUserDto>) {
+    return this.prismaService.user.update({
+      where: { id },
+      data: {
+        fullName: updateUserDto.fullName,
+        email: updateUserDto.email,
+        password: updateUserDto.password,
+        role: updateUserDto.role,
+      },
+    });
+  }
+
+  async remove(id: string) {
+    return this.prismaService.user.delete({
+      where: { id },
+    });
+  }
+}
